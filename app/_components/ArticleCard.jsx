dc4f1822eef5628127a2b5ba3c0b8c6f8fd087a9
Jorge Martin <jorgemf2604@gmail.com>
2023-10-11 09:54:05 +0100

    change minicards to color

--- app/_components/ArticleCard.jsx
+++ app/_components/ArticleCard.jsx
@@ -2,19 +2,27 @@ import Image from "next/image";
 import { AiFillHeart } from "react-icons/ai";
 
 const ArticleCard = ({ title, topic, author, votes, article_img_url }) => {
+  let colorLabelClass = null;
+  if (topic === "coding") {
+    colorLabelClass = "coding-label";
+  } else if (topic === "football") {
+    colorLabelClass = "football-label";
+  } else {
+    colorLabelClass = "cooking-label";
+  }
+
+  // TODO add filter by topic
+  // TODO add filter by date, number of votes, number of comments, asc, desc
+  // TODO link every article with a full article route
+  // TODO create full article page: show article, show comments, create a addComment form component, create a delete comment button
   return (
-    // TODO change the article cards to color
-    // TODO link every article with a full article route
-    // TODO add filter by topic
-    // TODO add filter by date, number of votes, number of comments, asc, desc
-    // TODO create full article page: show article, show comments, create a addComment form component, create a delete comment button
     <div className="group relative border-4 border-black p-4 rounded-md dark:bg-neutral-800/60 flex flex-col gap-4 hover:scale-105 hover:dark:bg-white hover:dark:text-neutral-600  hover:bg-neutral-800 hover:text-white transition">
       <div className="h-[250px] relative">
         <Image
           src={article_img_url}
           alt={title}
           fill
-          className="object-cover grayscale group-hover:grayscale-0 rounded"
+          className="object-cover rounded"
         />
       </div>
       <div>
@@ -22,11 +30,11 @@ const ArticleCard = ({ title, topic, author, votes, article_img_url }) => {
         <p className="text-sm mb-8 mt-2">by {author}</p>
       </div>
       <div className="flex justify-between items-center mt-auto">
-        <span className=" py-1 px-2 bg-black text-white rounded group-hover:bg-sky-500">
+        <span className={`py-1 px-2 rounded text-white ${colorLabelClass}`}>
           {topic}
         </span>
         <span className="flex gap-1 items-center">
-          <AiFillHeart className="group-hover:text-red-600" />
+          <AiFillHeart className="text-red-600" />
           {votes}
         </span>
       </div>
